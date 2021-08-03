c1a1eb74bf32f85807a2b98a9dba3e07bb486da9
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-03 14:18:54 +0200

    all process.env should be used in single config module

--- 2.5/index.js
+++ 2.5/index.js
@@ -1,3 +1,11 @@
-require("./util");
+const { config } = require("./util");
 
-console.log(`answer = `, answer)
+// to check that global variables are read anywhere
+console.log(`answer = `, answer);
+
+// instead of using directly
+process.env.PORT;
+
+// we should use it from config module as this is better for managing them
+
+console.log("process.env.PORT = ", config.port);
--- 2.5/util.js
+++ 2.5/util.js
@@ -1,3 +1,7 @@
 // if answer is defined as variable, it will be visible only inside this util module
 
 globalThis.answer = 42;
+
+exports.config = {
+  port: process.env.PORT || 8000,
+};
